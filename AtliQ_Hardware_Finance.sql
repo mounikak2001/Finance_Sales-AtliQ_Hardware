0beadf1740b4ef440aa6ebe01f187196a00161e4
Mounika Kalva <mounikaraok0105@gmail.com>
2024-06-23 19:30:29 +0530

    Add files via upload

---
+++ AtliQ_Hardware_Finance.sql
@@ -0,0 +1,73 @@
+### Module: User-Defined SQL Functions
+
+-- a. first grab customer codes for Croma india
+	SELECT * FROM dim_customer WHERE customer like "%croma%" AND market="india";
+
+-- b. Get all the sales transaction data from fact_sales_monthly table for that customer(croma: 90002002) in the fiscal_year 2021
+	SELECT * FROM fact_sales_monthly 
+	WHERE 
+            customer_code=90002002 AND
+            YEAR(DATE_ADD(date, INTERVAL 4 MONTH))=2021 
+	ORDER BY date asc
+	LIMIT 100000;
+
+-- c. Replacing the function created in the step:b
+	SELECT * FROM fact_sales_monthly 
+	WHERE 
+            customer_code=90002002 AND
+            get_fiscal_year(date)=2021 
+	ORDER BY date asc
+	LIMIT 100000;
+
+
+
+### Module: Gross Sales Report: Monthly Product Transactions
+
+-- a. Perform joins to pull product information
+	SELECT s.date, s.product_code, p.product, p.variant, s.sold_quantity 
+	FROM fact_sales_monthly s
+	JOIN dim_product p
+        ON s.product_code=p.product_code
+	WHERE 
+            customer_code=90002002 AND 
+    	    get_fiscal_year(date)=2021     
+	LIMIT 1000000;
+
+-- b. Performing join with 'fact_gross_price' table with the above query and generating required fields
+	SELECT 
+    	    s.date, 
+            s.product_code, 
+            p.product, 
+            p.variant, 
+            s.sold_quantity, 
+            g.gross_price,
+            ROUND(s.sold_quantity*g.gross_price,2) as gross_price_total
+	FROM fact_sales_monthly s
+	JOIN dim_product p
+            ON s.product_code=p.product_code
+	JOIN fact_gross_price g
+            ON g.fiscal_year=get_fiscal_year(s.date)
+    	AND g.product_code=s.product_code
+	WHERE 
+    	    customer_code=90002002 AND 
+            get_fiscal_year(s.date)=2021     
+	LIMIT 1000000;
+
+
+
+
+### Module: Gross Sales Report: Total Sales Amount
+
+-- Generate monthly gross sales report for Croma India for all the years
+	SELECT 
+            s.date, 
+    	    SUM(ROUND(s.sold_quantity*g.gross_price,2)) as monthly_sales
+	FROM fact_sales_monthly s
+	JOIN fact_gross_price g
+        ON g.fiscal_year=get_fiscal_year(s.date) AND g.product_code=s.product_code
+	WHERE 
+             customer_code=90002002
+	GROUP BY date;
+
+
+
---
+++ AtliQ_Hardware_Pj_2.sql
@@ -0,0 +1,174 @@
+### Module: Problem Statement and Pre-Invoice Discount Report
+
+-- Include pre-invoice deductions in Croma detailed report
+	SELECT 
+    	   s.date, 
+           s.product_code, 
+           p.product, 
+	   p.variant, 
+           s.sold_quantity, 
+           g.gross_price as gross_price_per_item,
+           ROUND(s.sold_quantity*g.gross_price,2) as gross_price_total,
+           pre.pre_invoice_discount_pct
+	FROM fact_sales_monthly s
+	JOIN dim_product p
+            ON s.product_code=p.product_code
+	JOIN fact_gross_price g
+    	    ON g.fiscal_year=get_fiscal_year(s.date)
+    	    AND g.product_code=s.product_code
+	JOIN fact_pre_invoice_deductions as pre
+            ON pre.customer_code = s.customer_code AND
+            pre.fiscal_year=get_fiscal_year(s.date)
+	WHERE 
+	    s.customer_code=90002002 AND 
+    	    get_fiscal_year(s.date)=2021     
+	LIMIT 1000000;
+
+-- Same report but all the customers
+	SELECT 
+    	   s.date, 
+           s.product_code, 
+           p.product, 
+	   p.variant, 
+           s.sold_quantity, 
+           g.gross_price as gross_price_per_item,
+           ROUND(s.sold_quantity*g.gross_price,2) as gross_price_total,
+           pre.pre_invoice_discount_pct
+	FROM fact_sales_monthly s
+	JOIN dim_product p
+            ON s.product_code=p.product_code
+	JOIN fact_gross_price g
+    	    ON g.fiscal_year=get_fiscal_year(s.date)
+    	    AND g.product_code=s.product_code
+	JOIN fact_pre_invoice_deductions as pre
+            ON pre.customer_code = s.customer_code AND
+            pre.fiscal_year=get_fiscal_year(s.date)
+	WHERE 
+    	    get_fiscal_year(s.date)=2021     
+	LIMIT 1000000;
+
+
+
+
+
+### Module: Performance Improvement # 1
+
+-- creating dim_date and joining with this table and avoid using the function 'get_fiscal_year()' to reduce the amount of time taking to run the query
+	SELECT 
+    	    s.date, 
+            s.customer_code,
+            s.product_code, 
+            p.product, p.variant, 
+            s.sold_quantity, 
+            g.gross_price as gross_price_per_item,
+            ROUND(s.sold_quantity*g.gross_price,2) as gross_price_total,
+            pre.pre_invoice_discount_pct
+	FROM fact_sales_monthly s
+	JOIN dim_date dt
+        	ON dt.calendar_date = s.date
+	JOIN dim_product p
+        	ON s.product_code=p.product_code
+	JOIN fact_gross_price g
+    		ON g.fiscal_year=dt.fiscal_year
+    		AND g.product_code=s.product_code
+	JOIN fact_pre_invoice_deductions as pre
+        	ON pre.customer_code = s.customer_code AND
+    		pre.fiscal_year=dt.fiscal_year
+	WHERE 
+    		dt.fiscal_year=2021     
+	LIMIT 1500000;
+
+
+
+
+
+
+### Module: Performance Improvement # 2
+
+-- Added the fiscal year in the fact_sales_monthly table itself
+	SELECT 
+    	    s.date, 
+            s.customer_code,
+            s.product_code, 
+            p.product, p.variant, 
+            s.sold_quantity, 
+            g.gross_price as gross_price_per_item,
+            ROUND(s.sold_quantity*g.gross_price,2) as gross_price_total,
+            pre.pre_invoice_discount_pct
+	FROM fact_sales_monthly s
+	JOIN dim_product p
+        	ON s.product_code=p.product_code
+	JOIN fact_gross_price g
+    		ON g.fiscal_year=s.fiscal_year
+    		AND g.product_code=s.product_code
+	JOIN fact_pre_invoice_deductions as pre
+        	ON pre.customer_code = s.customer_code AND
+    		pre.fiscal_year=s.fiscal_year
+	WHERE 
+    		s.fiscal_year=2021     
+	LIMIT 1500000;
+
+
+
+
+
+### Module: Database Views: Introduction
+
+-- Get the net_invoice_sales amount using the CTE's
+	WITH cte1 AS (
+		SELECT 
+    		    s.date, 
+    		    s.customer_code,
+    		    s.product_code, 
+                    p.product, p.variant, 
+                    s.sold_quantity, 
+                    g.gross_price as gross_price_per_item,
+                    ROUND(s.sold_quantity*g.gross_price,2) as gross_price_total,
+                    pre.pre_invoice_discount_pct
+		FROM fact_sales_monthly s
+		JOIN dim_product p
+        		ON s.product_code=p.product_code
+		JOIN fact_gross_price g
+    			ON g.fiscal_year=s.fiscal_year
+    			AND g.product_code=s.product_code
+		JOIN fact_pre_invoice_deductions as pre
+        		ON pre.customer_code = s.customer_code AND
+    			pre.fiscal_year=s.fiscal_year
+		WHERE 
+    			s.fiscal_year=2021) 
+	SELECT 
+      	    *, 
+    	    (gross_price_total-pre_invoice_discount_pct*gross_price_total) as net_invoice_sales
+	FROM cte1
+	LIMIT 1500000;
+
+
+-- Creating the view `sales_preinv_discount` and store all the data in like a virtual table
+	CREATE  VIEW `sales_preinv_discount` AS
+	SELECT 
+    	    s.date, 
+            s.fiscal_year,
+            s.customer_code,
+            c.market,
+            s.product_code, 
+            p.product, 
+            p.variant, 
+            s.sold_quantity, 
+            g.gross_price as gross_price_per_item,
+            ROUND(s.sold_quantity*g.gross_price,2) as gross_price_total,
+            pre.pre_invoice_discount_pct
+	FROM fact_sales_monthly s
+	JOIN dim_customer c 
+		ON s.customer_code = c.customer_code
+	JOIN dim_product p
+        	ON s.product_code=p.product_code
+	JOIN fact_gross_price g
+    		ON g.fiscal_year=s.fiscal_year
+    		AND g.product_code=s.product_code
+	JOIN fact_pre_invoice_deductions as pre
+        	ON pre.customer_code = s.customer_code AND
+    		pre.fiscal_year=s.fiscal_year
+
+
+
+
